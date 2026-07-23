6325ba7d979e233cb0cfe8f0c13b35fa77c02a87
romain4745 <barakaromain25@gmail.com>
2026-07-23 17:01:55 +0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,6 @@
-#!/bin/bash
-
-echo "Starting IntelTrace WSGI Server..."
-
-exec gunicorn \
+gunicorn \
     --workers 4 \
     --threads 2 \
-    --bind 127.0.0.1:5000 \
+    --bind 0.0.0.0:5000 \   # Changed from 127.0.0.1
     --timeout 120 \
     wsgi:application
